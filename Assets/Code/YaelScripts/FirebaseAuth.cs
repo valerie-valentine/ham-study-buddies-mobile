bbfcc531fbfc617d04b036fdf02a0416c5afa5e6
Yael Peet <yaelpeet@gmail.com>
2023-08-02 15:59:57 -0700

    firebase auth game object, script, in sign up scene

---
+++ Assets/Code/YaelScripts/FirebaseAuth.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Auth;
+using Firebase.Extensions;
+using Firebase;
+
+public class FirebaseAuthentication : MonoBehaviour
+{
+    FirebaseAuth auth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnableAuthentication();
+
+        SignUp("newemail@gmail.com", "123456");
+    }
+
+    // Update is called once per frame
+    void EnableAuthentication()
+    {
+        //Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth = FirebaseAuth.DefaultInstance;
+    }
+
+    internal void SignUp(string email, string password)
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.AuthResult result = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                result.User.DisplayName, result.User.UserId);
+        });
+
+    }
+
+    public void SignIn(string email, string password)
+    {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.AuthResult result = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                result.User.DisplayName, result.User.UserId);
+        });
+
+
+
+
+    }
+
+    public void SignOut()
+    {
+
+    }
+}
