08b1464f92337916f8fd4022147bc22305118e24
Yvett Jimenez <yvett.nez@gmail.com>
2023-08-02 17:54:02 -0500

    edited user sign up script

--- Assets/Code/YvettScripts/FirebaseAuthentication.cs
+++ Assets/Code/YvettScripts/FirebaseAuthentication.cs
@@ -9,7 +9,7 @@ public class FirebaseAuthentication : MonoBehaviour
     {
         EnableAuthentication();
 
-        SignUp("yvett@gmail.com", "123456");
+        SignUp("gitcommitme@gmail.com", "123456");
     }
 
     // Update is called once per frame
