e9c83f5995dd9a0a66204d0b7bf34e5e28481906
Yvett Jimenez <yvett.nez@gmail.com>
2023-08-04 23:25:08 -0500

    added audio scripts and pseudocode for accessing AudioManager object, added Audio tag to object

--- Assets/Code/YvettScripts/AudioManager.cs
+++ Assets/Code/YvettScripts/AudioManager.cs
@@ -16,4 +16,22 @@ public class AudioManager : MonoBehaviour
         musicSource.clip= background;
         musicSource.Play();
     }
+
+    // to be called by other scripts, plays SFX
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
 }
+
+
+// FIRST --- in other scripts, add this:
+// AudioManager audioManager;
+
+// SECOND --- inside private void Awake()
+// {
+//      audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+// }
+
+// THIRD --- go to where you want to call the function
+// audioManager.PlaySFX(audioManager.nameOfSFX1);
