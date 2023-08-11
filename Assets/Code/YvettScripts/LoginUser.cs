745ee69b8b27cfd7218afa9f5444914d5ca9bc44
Yvett Jimenez <yvett.nez@gmail.com>
2023-08-10 22:30:08 -0500

    created login user script to refactor authmanager

---
+++ Assets/Code/YvettScripts/LoginUser.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using TMPro;
+using System.Threading.Tasks;
+using Firebase.Firestore;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class LoginUser : MonoBehaviour
+{
+    //Firebase variables
+    [Header("Firebase")]
+    public FirebaseAuth auth;
+    public FirebaseUser User;
+
+    //Login variables
+    [Header("Login")]
+    public TMP_InputField emailLoginField;
+    public TMP_InputField passwordLoginField;
+    public TMP_Text warningLoginText;
+    public TMP_Text confirmLoginText;
+    public TMP_Text welcomeLoginText;
+
+    FirebaseFirestore db;
+
+
+    void Awake()
+    {
+        db = FirebaseFirestore.DefaultInstance;
+        Debug.Log($"Auth manager instance ID: {gameObject.GetInstanceID()}");
+
+        // added variables
+        auth = AuthManager.instance.auth;
+        User = AuthManager.instance.User;
+    }
+
+    //Function for the login button
+    public void LoginButton()
+    {
+        //Call the login coroutine passing the email and password
+        StartCoroutine(Login(emailLoginField.text, passwordLoginField.text));
+    }
+
+    private IEnumerator Login(string _email, string _password)
+    {
+        //Call the Firebase auth signin function passing the email and password
+        Task<AuthResult> LoginTask = auth.SignInWithEmailAndPasswordAsync(_email, _password);
+        //Wait until the task completes
+        yield return new WaitUntil(predicate: () => LoginTask.IsCompleted);
+
+        if (LoginTask.Exception != null)
+        {
+            //If there are errors handle them
+            Debug.LogWarning(message: $"Failed to register task with {LoginTask.Exception}");
+            FirebaseException firebaseEx = LoginTask.Exception.GetBaseException() as FirebaseException;
+            AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
+
+            string message = "Login Failed!";
+            switch (errorCode)
+            {
+                case AuthError.MissingEmail:
+                    message = "Missing Email";
+                    break;
+                case AuthError.MissingPassword:
+                    message = "Missing Password";
+                    break;
+                case AuthError.WrongPassword:
+                    message = "Wrong Password";
+                    break;
+                case AuthError.InvalidEmail:
+                    message = "Invalid Email";
+                    break;
+                case AuthError.UserNotFound:
+                    message = "Account does not exist";
+                    break;
+            }
+            warningLoginText.text = message;
+        }
+        else
+        {
+            //User is now logged in
+            //Now get the result
+            User = new FirebaseUser(LoginTask.Result.User);
+            Debug.LogFormat("User signed in successfully: {0} ({1})", User.DisplayName, User.Email);
+            warningLoginText.text = "";
+            confirmLoginText.text = "Logged In";
+            welcomeLoginText.text = $"Welcome {User.DisplayName}";
+            ScenesManager.Instance.LoadMainPage();
+        }
+    }
+}
