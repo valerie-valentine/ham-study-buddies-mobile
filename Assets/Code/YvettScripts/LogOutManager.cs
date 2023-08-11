df0d595e9751131cb9aeb0f781bae1f35165a4ac
Yvett Jimenez <yvett.nez@gmail.com>
2023-08-10 22:26:40 -0500

    cleaned up logout manager script

--- Assets/Code/YvettScripts/LogOutManager.cs
+++ Assets/Code/YvettScripts/LogOutManager.cs
@@ -5,23 +5,16 @@ using Firebase.Auth;
 
 public class LogOutManager : MonoBehaviour
 {
-    FirebaseUser currentUser;
     public FirebaseAuth auth;
-
-    void Awake()
-    {
-        currentUser = AuthManager.instance.User;
-        auth = AuthManager.instance.auth;
-        
-    }
 
     public void Logout()
     {
-        string displayName = currentUser.DisplayName;
+        auth = AuthManager.instance.auth;
+
         if (auth != null)
         {
             auth.SignOut();
-            Debug.Log($"User {displayName} signed out!");
+            Debug.Log($"User signed out!");
             ScenesManager.Instance.LoadStartPage();
         }
         else
