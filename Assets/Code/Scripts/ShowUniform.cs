7a633bd2e5265f0b260994df30d8c80c48da6f2f
Yael Peet <yaelpeet@gmail.com>
2023-08-14 12:26:13 -0700

    new uniform animation

---
+++ Assets/Code/Scripts/ShowUniform.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUniform : MonoBehaviour
+{
+    public GameObject objectToShow;
+
+    public void ShowAndHideObject()
+    {
+        StartCoroutine(ShowObjectCoroutine());
+    }
+
+    private IEnumerator ShowObjectCoroutine()
+    {
+        objectToShow.SetActive(true);
+
+        yield return new WaitForSeconds(1.2f); // Show the object for 1 second
+
+        objectToShow.SetActive(false);
+    }
+}
+
