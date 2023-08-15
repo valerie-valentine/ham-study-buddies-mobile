ab6deb50ab4bdf30974dfb7395bf3b7121336cea
Yvett Jimenez <yvett.nez@gmail.com>
2023-08-14 21:03:00 -0500

    created signin page script and attached button sfx

---
+++ Assets/Code/Scripts/Audio/SFXSignInScene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXSignInScene : MonoBehaviour
+{
+    AudioManager audioManager;
+
+    void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
+
+    public void PlayButtonClickSFX()
+    {
+        //SFXSource.PlayOneShot(buttonClickSFX);
+        audioManager.PlaySFX(audioManager.buttonClickSFX);
+    }
+}
